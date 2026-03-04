d472b7d0474c99eb4132f78c2260af08cdf14db6
EduDeveloper8145 <pigkitspvp8145@gmail.com>
2026-03-04 21:24:52 +0000

    adicionado função de ativar

--- ep1.py
+++ ep1.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = []
+restaurantes = [{'nome':'GildoLanches', 'categoria':'Fast-Food', 'ativo': True},
+                {'nome': 'Pizzaria do Gordo', 'categoria': 'Pizza', 'ativo': False},
+                {'nome': 'Cafetao Cafeteria', 'categoria': 'Cafeteria', 'ativo': True}]
 
 def exibir_logo_programa():
     print("""
@@ -19,27 +21,52 @@ def exibir_opcoes():
     print('3. Ativar Restaurante')
     print('4. Sair\n')
 
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal! ')
+    main()
+
+def exibir_subtitulos(texto):
+    os.system('clear')
+    print(texto)
+
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal.')
-    main()
+    voltar_ao_menu_principal()
 
 def cadastrar_novo_restaurante():
-    os.system('clear')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulos('Cadastro de Novos Restaurantes\n')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal! ')
-    main()
+    voltar_ao_menu_principal()
 
 def listar_novos_restaurantes():
-    os.system('clear')
-    for i in restaurantes:
-        print(f'.{i}')
+    exibir_subtitulos('Listagem de Restaurantes')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria =  restaurante['categoria']
+        ativo = 'ativado' if i['ativo'] else 'desativado'
+        print(f'- NOME: {nome_restaurante} | CATEGORIA: {categoria} | SITUACAO: {ativo}')
+    voltar_ao_menu_principal()
 
-    input('Digite uma tecla para voltar ao menu principal! ')
-    main()
+def alterando_estado_do_restaurante():
+    exibir_subtitulos('Atualizando Estado do Restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja ativar/desativar estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso.' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso.'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print(f'Infelizmente o restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
 
@@ -52,21 +79,21 @@ def escolher_opcao():
             print('Ativacao de Restaurante')
 
         def finalizar_app():
-            os.system('clear')
-            print('Você saiu!')
+            exibir_subtitulos('Finalizar Programa!')
         
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
             listar_novos_restaurantes()
         elif opcao_escolhida == 3:
-            ativar_app()
+            alterando_estado_do_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
             opcao_invalida()
     except:
         opcao_invalida()
+        
 def main():
     os.system('clear')
     exibir_logo_programa()
@@ -80,3 +107,6 @@ if __name__ == '__main__':
 
 
 #Interpolar: juntar um texto com variáveis (formatação f-string)
+#Tupla: são arrays imutáveis, para situações que precisam armazenar algo e não modificar. Ex.: coordenadas geográficas. Utilizada usando ()
+#Lista: são arrays mutáveis, para situações que podem sofrer modificações. Ex.: lista de compras. Utilizada usando []
+# For deve ser utilizado quando se tem um número determinado de tentativas, já o while é utilizado para casos indeterminados.
